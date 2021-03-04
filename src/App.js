3fb8b3c9359b551adf195364ff06eebabea12eb4
Dilan Silva <sdilansilva@gmail.com>
2021-03-04 20:27:06 +0530

    Add Bootstrap and navigation to project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <LoginPage/>
     </div>
   );
 }
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginPage = () => {
+    return(
+        <div>
+            <h1>Login Pag e</h1>
+        </div>
+    );
+}
+
+export default LoginPage;
