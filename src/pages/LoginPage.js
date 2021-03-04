b5e7993d2625ecd9ee178be354e22e16c62383ce
Dilan Silva <sdilansilva@gmail.com>
2021-03-04 20:45:47 +0530

    Login Page UI is done

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
+import { Form,  //Import some tags from bootstrap
+        Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';//bootstrap
+
+import '../css/LoginPageCss.css'//custom css
+ 
 const LoginPage = () => {
     return(
-        <div>
-            <h1>Login Pag e</h1>
+        <div className="d-flex justify-content-center BottomWidth">
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>User Name</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+      
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Log-In
+                </Button>
+            </Form>
         </div>
     );
 }
